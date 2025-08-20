b2da38ee873b6836f3aef90fa1c59b5edcbacfb7
NikolaNeitchev <134317563+NikolaNeitchev@users.noreply.github.com>
2025-08-20 23:27:44 +0300

    Update Service.js

--- backend/models/Service.js
+++ backend/models/Service.js
@@ -2,16 +2,22 @@ const { client } = require('../config/database');
 
 const Service = {
   async create({ title, description, price, userId }) {
-    const result = await client.query(
+    const { rows } = await client.query(
       'INSERT INTO "Services" (title, description, price, "userId") VALUES ($1, $2, $3, $4) RETURNING *',
       [title, description, price, userId]
     );
-    return result.rows[0];
+    return rows[0];
   },
+
   async findAll() {
-    const result = await client.query('SELECT * FROM "Services"');
-    return result.rows;
-  }
+    const { rows } = await client.query('SELECT * FROM "Services" ORDER BY id ASC');
+    return rows;
+  },
+
+  async findById(id) {
+    const { rows } = await client.query('SELECT * FROM "Services" WHERE id = $1', [id]);
+    return rows[0];
+  },
 };
 
 module.exports = Service;
